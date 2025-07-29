893e79e2fa9c62cc8441d27ea9f9ef20e7ffe442
Xhulian <xhulianazemi9@gmail.com>
2025-07-29 13:59:11 +0200

    feat: Implement add task functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+let inputElement = document.getElementById("input");
+const addButton = document.getElementById("add-task-button");
+let ul = document.getElementById("list");
+
+function addTask() {
+  let inputValue = inputElement.value;
+  if (inputValue.trim() === '') {
+    return
+  }
+  let listItem = document.createElement("li");
+  listItem.textContent = inputValue;
+  ul.appendChild(listItem);
+  inputElement.value = '';
+}
+
+addButton.addEventListener("click", () => { addTask(); });
+
+inputElement.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+
