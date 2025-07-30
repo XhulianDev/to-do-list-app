56150f556291e153fa09865cad24aa2749534113
Xhulian <xhulianazemi9@gmail.com>
2025-07-30 17:32:42 +0200

    feat: persist tasks using local storage

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function addTask() {
   listItem.appendChild(removeTask);
   ul.appendChild(listItem);
   inputElement.value = '';
+  saveData();
 }
 
 addButton.addEventListener("click", () => { addTask(); });
@@ -29,9 +30,11 @@ inputElement.addEventListener("keydown", (event) => {
 ul.addEventListener("click", (event) => {
   if (event.target.tagName === "LI") {
     event.target.classList.toggle("completed");
+    saveData();
   } else if (event.target.className === "delete-button") {
     let li = event.target.parentNode;
     li.remove();
+    saveData();
   }
 });
 
@@ -40,4 +43,48 @@ removeAll.addEventListener("click", () => {
   completedTasks.forEach((task) => {
     task.remove();
   });
+  saveData();
+});
+
+function saveData() {
+  const allTasks = document.querySelectorAll("li");
+  let tasksToSave = [];
+  
+  allTasks.forEach((task) => {
+    const taskData = {
+      text: task.textContent.replace('X', ''),
+      completed: task.classList.contains("completed")
+    };
+    tasksToSave.push(taskData);
+  });
+  
+  let dataToSave = JSON.stringify(tasksToSave);
+  localStorage.setItem("data", dataToSave);
+}
+
+function showTask() {
+  let storedData = localStorage.getItem("data");
+  let tasks = JSON.parse(storedData) || [];
+  
+  ul.innerHTML = "";
+  
+  tasks.forEach((taskData) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = taskData.text;
+    
+    if (taskData.completed) {
+      listItem.classList.add("completed");
+    }
+    
+    const removeTask = document.createElement("span");
+    removeTask.className = "delete-button";
+    removeTask.textContent = 'X';
+    
+    listItem.appendChild(removeTask);
+    ul.appendChild(listItem);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showTask();
 });
